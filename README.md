45246638979983f14595c4cc7fbbc2363b14c207
junjie-lean <junjie.lean@foxmail.com>
2021-11-07 20:23:22 +0800

    Celebrate

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Celebrate EDG winning the league of Legends S11 championship in 2021.
+
+## We are the Champion!
---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: junjie.lean 
+ * @Date: 2021-11-07 20:16:07 
+ * @Last Modified by: junjie.lean
+ * @Last Modified time: 2021-11-07 20:20:10
+ */
+
+
+const celebrate2021 = ()=>{
+    console.log(
+        'Celebrate EDG winning the league of Legends S11 championship in 2021'
+    )
+}
+
+module.exports = {
+    celebrate2021
+}
